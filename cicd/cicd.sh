3d527f92d9f2cdc2e86eccb97965bdb7fd2048b0
Dan Saunders <danjsaund@gmail.com>
2025-01-27 14:36:01 +0000

    renaming file to prevent mypy conflict

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -3,4 +3,4 @@ set -e
 
 python -c "import torch; assert '$PYTORCH_VERSION' in torch.__version__"
 
-pytest -v --durations=10 -n8 /workspace/diff-transformer/tests/
+pytest -v --durations=10 -n8 /workspace/diff-transformer/cicd_tests/
